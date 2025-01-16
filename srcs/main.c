f54957e334cee3f77da312eb818f812f9b1016d6
AzerSD <sioudazer8@gmail.com>
2025-01-16 07:57:52 +0000

    Added FQDN Resolve

--- srcs/main.c
+++ srcs/main.c
@@ -24,7 +24,7 @@ static unsigned short calculate_checksum(void *b, int len)
     return result;
 }
 
-int main()
+int main(int argc, char *argv[], char *envp[])
 {
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sockfd < 0)
@@ -32,6 +32,23 @@ int main()
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
+
+
+    // Resolve FQDN to IP
+    struct addrinfo hints, *res;
+    char ip_str[INET_ADDRSTRLEN];
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_RAW;
+
+    if (getaddrinfo(argv[1], NULL, &hints, &res) != 0) {
+        perror("getaddrinfo failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
+    inet_ntop(AF_INET, &(addr->sin_addr), ip_str, sizeof(ip_str));
+    printf("Resolved IP for %s: %s\n", argv[1], ip_str);
 
     struct icmphdr icmp_hdr;
     icmp_hdr.type = ICMP_ECHO;
@@ -42,9 +59,11 @@ int main()
 
     icmp_hdr.checksum = calculate_checksum(&icmp_hdr, sizeof(icmp_hdr));
 
+
     struct sockaddr_in dest_addr;
     dest_addr.sin_family = AF_INET;
-    inet_pton(AF_INET, "8.8.8.8", &dest_addr.sin_addr);
+    dest_addr.sin_addr = addr->sin_addr;
+    // inet_pton(AF_INET, "8.8.8.8", &dest_addr.sin_addr);
 
     if (sendto(sockfd, &icmp_hdr, sizeof(icmp_hdr), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
         perror("Sendto failed");
